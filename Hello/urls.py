259f6d99257beb6d70b1cd039381c06c4c643356
shivamsengar135 <shivamswngar4321@gmail.com>
2026-03-25 10:43:15 +0530

    Prepare Django app for Render deployment

--- Hello/urls.py
+++ Hello/urls.py
@@ -21,9 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("home.urls")),
-    path("about/", include("home.urls")),
-    path("contact/", include("home.urls")),
-    path("services/", include("home.urls"))
 ]
 
 
